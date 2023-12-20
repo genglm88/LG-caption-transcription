cf51f3794a6c43e8e1b29d51f72a03085c3310e6
genglm88 <108308458+genglm88@users.noreply.github.com>
2023-12-20 12:56:23 -0500

    change key name

--- src/app/api/transcribe/route.js
+++ src/app/api/transcribe/route.js
@@ -9,7 +9,7 @@ const getClient = () => {
   return new TranscribeClient({
     region: "us-east-2",
     credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY,
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     },
   })
@@ -62,7 +62,7 @@ const getTranscriptionFile = async (filename) => {
   const s3client = new S3Client({
     region: "us-east-2",
     credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY,
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     },
   })
